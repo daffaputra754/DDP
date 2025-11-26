d455f6edb959147327bfdb022254304119aaba8e
daffaputra754 <mauladardaffaputra@gmail.com>
2025-11-26 11:01:33 +0700

    Add files via upload

---
+++ pratikum ddp 10/BangunDatar.py
@@ -0,0 +1,8 @@
+import BangunDatar as br
+
+print(" BANGUN DATAR ~~")
+print(f"Luas Persegi adalah ")
+print(f"Luas Persegi Panjang adalah ")
+print(f"Luas Segitiga adalah ")
+print(f"Luas Lingkaran adalah ")
+print(f"Luas JajarGanjar adalah ")
